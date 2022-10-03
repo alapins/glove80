46ce3b222911028ec7a85d46dd536c18d0e44ab0
Chris Andreae <chris@andreae.gen.nz>
2022-10-03 11:41:31 +0900

    Initial external config repository

--- config/default.nix
+++ config/default.nix
@@ -2,9 +2,8 @@
 
 let
   firmware = import ../src {};
-  config = ./.;
 
-  glove80_left  = firmware.zmk.override { board = "glove80_lh"; keymap = "${config}/glove80.keymap"; kconfig = "${config}/glove80.conf"; };
-  glove80_right = firmware.zmk.override { board = "glove80_rh"; keymap = "${config}/glove80.keymap"; kconfig = "${config}/glove80.conf"; };
+  glove80_left  = firmware.zmk.override { board = "glove80_lh"; keymap = ./glove80.keymap; };
+  glove80_right = firmware.zmk.override { board = "glove80_rh"; keymap = ./glove80.keymap; };
 
 in firmware.combine_uf2 glove80_left glove80_right
