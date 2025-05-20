df1ea62bc165ea1f35a9730c5e87c87831b62aa6
alapins <alex@lapins.com>
2025-05-20 16:39:56 -0400

    per key rgb

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@
 set -euo pipefail
 
 IMAGE=glove80-zmk-config-docker
-BRANCH="${1:-main}"
+# BRANCH="${1:-main}"
+BRANCH="${1:-pull/36/head}"
 
 docker build -t "$IMAGE" .
 docker run --rm -v "$PWD:/config" -e UID="$(id -u)" -e GID="$(id -g)" -e BRANCH="$BRANCH" "$IMAGE"
